31209836751d7b7b4f821785de91d0b411610874
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-05-06 09:42:25 -0500

    first commit

---
+++ Ships.cs
@@ -0,0 +1,39 @@
+class Ship
+{
+
+}
+
+
+
+class Carrier : Ship
+{
+
+}
+
+
+
+class BattleShip : Ship
+{
+
+}
+
+
+
+class Cruiser : Ship
+{
+
+}
+
+
+
+class Destroyer : Ship
+{
+
+}
+
+
+
+class Submarine : Ship
+{
+    
+}
