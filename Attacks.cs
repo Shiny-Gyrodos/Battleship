74f7f119eff7c5d4da5ffb3ff8db289aec206d98
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-06-21 12:13:59 -0500

    Finished PlaceRadar method. Began implementation of StripBomb method.

--- Attacks.cs
+++ Attacks.cs
@@ -16,6 +16,7 @@ abstract class Attacks // Potentially change from abstract in the future.
                     break;
                 case null: // If mine
                     chosenGrid[coord1, coord2] = new Node(false, true, '#', NodeTypes.other);
+                    MineDetonates(chosenGrid);
                     break;
             }
 
@@ -27,7 +28,69 @@ abstract class Attacks // Potentially change from abstract in the future.
 
 
 
-    public static void MineDetonates(Node[,] chosenGrid)
+    public static void StripBomb(Node[,] chosenGrid)
+    {
+        bool validInputRecieved = false;
+
+        while (!validInputRecieved)
+        {
+            Console.WriteLine("\nInput the key of the column or row you wish to strip bomb.");
+
+            try
+            {
+                char playerInput = Console.ReadKey().KeyChar;
+
+                // 49-56 are the char values of numbers 1-8. 65-72 are the char values of A-H.
+                if (playerInput >= 49 && playerInput <= 56)
+                {
+                    // Bombing a row, playerInput - 1, perform int cast
+                }
+                else if (playerInput >= 65 && playerInput <= 72)
+                {
+                    // Bombing a column, playerInput - 65, subtract 65 and work with the number from there
+                }
+            }
+            catch {} // Add the error code later.
+        }
+    }
+
+
+
+    public static bool PlaceRadar(Node[,] chosenGrid, int coord1, int coord2)
+    {
+        // If statement is checking whether the spot chosen already has a radar placed on it or not.
+        // 48 is the number 0 in the ASCII table, and 57 is the number 9. ASCII table link : https://www.asciitable.com
+        // Radars can't be placed on nodes you've already fired at.
+        if (chosenGrid[coord1, coord2].Icon >= 48 && chosenGrid[coord1, coord2].Icon <= 57 && !chosenGrid[coord1, coord2].FiredAt)
+        {
+            return false;
+        }
+
+        int detectedObjects = 0;
+
+        // Searches a 3x3 grid around where the radar is placed for occupied nodes.
+        for (int i = -1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+            {
+                try 
+                { 
+                    if (chosenGrid[coord1 + i, coord2 + j].NodeFilled != false)
+                    {
+                        detectedObjects++;
+                    }
+                }
+                catch (IndexOutOfRangeException) { }
+            }
+        }
+
+        chosenGrid[coord1, coord2].Icon = (char)detectedObjects;
+        return true;
+    }
+
+
+
+    static void MineDetonates(Node[,] chosenGrid)
     {
         for (int validNodesFound = 0; validNodesFound < 2; )
         {
