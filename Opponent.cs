e20e1f3dc9f610e8791d01d9a27aba435bcac943
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-06-30 23:16:10 -0500

    Removed/added some comments. Began some implementation. Refactored some code.

--- Opponent.cs
+++ Opponent.cs
@@ -4,9 +4,8 @@ using System.Linq.Expressions;
 class Opponent : Attacks
 {
     // Consider looking into adding a constructor class.
-    // Consider adding a list named something like "queuedCoordPairs" that stores coord pairs that need the surrounding area checked.
     static List<(int, int)> successfulCoordPairsShotAt = [];
-    static Queue<(int, int)>[] coordSearchQueue = [[], []]; // The first list is for positively modified coords while the second is for negatively.
+    static Queue<(int, int)>[] coordSearchQueue = [[], []]; // The first queue is for positively modified coords while the second is for negatively.
     static Random rng = new();
     static (int vertical, int horizontal) coords = (rng.Next(0, 8), rng.Next(0, 8));
     static int emptyNodesFiredAt = 0;
@@ -17,21 +16,31 @@ class Opponent : Attacks
         {
             case 1:
                 turnPhase = HuntForShipSegments(grids) ? turnPhase + 1 : turnPhase;
+                turnPhase = Ship.CheckForDestroyed() == null ? turnPhase : 1;
                 break;
             case 2:
                 turnPhase = FindShipOrientation(grids) ? turnPhase + 1 : turnPhase;
+                turnPhase = Ship.CheckForDestroyed() == null ? turnPhase : 1;
                 break;
             case 3:
-                // Code that manages calling the phase 3 methods.
+                if (coordSearchQueue[0].Count + coordSearchQueue[1].Count == 0)
+                {
+                    AddPossibleCoords();
+                }
+
+                if (emptyNodesFiredAt == 2 || Ship.CheckForDestroyed() == null)
+                {
+                    turnPhase = 1;
+                    break;
+                }
+                FinishingOffShip(grids);
                 break;
         }
-
-        // Check if ship sunk == true. If so then revert to phase 1, unless there are more coords in the queue (or something like that).
     }
 
 
     // This method is used when the ships's orientation has been found.
-    static void FinishingOffShip(Grid grids) // Haven't yet accounted for when the first two coord pairs acquired belong to different ships.
+    static void FinishingOffShip(Grid grids) // Could be improved by accounting for when the ai finds two ships next to each other.
     {
         bool validNodeFiredAt = false;
 
@@ -52,31 +61,24 @@ class Opponent : Attacks
 
 
 
-    static void AddPossibleCoords(int howMany)
+    static void AddPossibleCoords()
     { 
-        if (successfulCoordPairsShotAt[0].Item1 == successfulCoordPairsShotAt[1].Item1)
+        for (int i = 0; i < 2; i++)
         {
-            for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 3; j++)
             {
-                for (int j = 0; j < howMany; j++)
+                if (successfulCoordPairsShotAt[0].Item1 == successfulCoordPairsShotAt[1].Item1)
                 {
                     // If i == 1, then make the coord negative.
                     coordSearchQueue[i].Enqueue((successfulCoordPairsShotAt[0].Item1, (successfulCoordPairsShotAt[0].Item2 + j + 1) * i == 1 ? -1 : 1));
                 }
-            }
-        }
-        else
-        {
-            for (int i = 0; i < 2; i++)
-            {
-                for (int j = 0; j < howMany; j++)
+                else
                 {
-                    // Same as above, except altering the other coord.
+                    // Same as above, but altering the other coord.
                     coordSearchQueue[i].Enqueue(((successfulCoordPairsShotAt[0].Item1 + j + 1) * i == 1 ? -1 : 1, successfulCoordPairsShotAt[0].Item2));
                 }
             }
-        }
-        
+        } 
     }
 
 
