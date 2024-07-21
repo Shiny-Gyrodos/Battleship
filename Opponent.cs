decad160ec8d3ea434c598be9d75e6669dba6369
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-07-20 23:54:01 -0500

    Huge refactor. Not finished yet, but neering completion. Other files will need some changes to accommodate new refactor.

--- Opponent.cs
+++ Opponent.cs
@@ -1,120 +1,93 @@
 using System.Diagnostics;
 using System.Linq.Expressions;
 
-class Opponent : Attacks
+abstract class Opponent : Attacks
 {
-    // Consider looking into adding a constructor class.
-    static List<NodeTypes>[] shipsLeft = [[], [], [], [], [], []];
-    static List<(int, int)> sucShots = [];
-    static Queue<(int, int)>[] coordSearchQueue = [[], []]; // The first queue is for positively modified coords while the second is for negatively.
     static readonly Random rng = new();
-    static (int column, int row) coords = (rng.Next(0, 8), rng.Next(0, 8));
-    static int emptyNodesFiredAt = 0;
-    static int turnPhase = 1;
-    public static void TakeTurn(Grid grids)
+    public static void TakeTurn(Grid grids, Brain brain)
     {
-        switch (turnPhase)
+        switch (brain.turnPhase)
         {
             case 1:
-                if (FireRandomly(grids))
+                if (Loop(Shoot, GetRandomCoords, grids.playerGrid, brain) is ((int, int), bool) data1 && data1.shotShip)
                 {
-                    turnPhase++;
-                    sucShots.Add(coords);
-                    turnPhase = Ship.CheckForDestroyed(grids.playerGrid, shipsLeft) ? turnPhase : 1; // Null is placeholder
+                    brain.turnPhase++;
+                    brain.sucShots.Add(data1.coords);
+                    brain.turnPhase = Ship.CheckForDestroyed(grids.playerGrid, brain.shipsLeft) ? brain.turnPhase : 1;
                 }
                 break;
             case 2:
-                if (FindShipRotation(grids))
+                if (Loop(Shoot, GetAlteredCoords, grids.playerGrid, brain) is ((int, int), bool) data2 && data2.shotShip)
                 {
-                    turnPhase++;
-                    sucShots.Add(coords);
-                    turnPhase = Ship.CheckForDestroyed(grids.playerGrid, shipsLeft) ? turnPhase : 1; // Null is placeholder
+                    brain.turnPhase++;
+                    brain.sucShots.Add(data2.coords);
+                    brain.turnPhase = Ship.CheckForDestroyed(grids.playerGrid, brain.shipsLeft) ? brain.turnPhase : 1;
+                    AddPossibleCoords(brain);
                 }
                 break;
             case 3:
-                if (coordSearchQueue[0].Count + coordSearchQueue[1].Count == 0)
+                if (!Loop(Shoot, GetQueuedCoords, grids.playerGrid, brain, 2).shotShip)
                 {
-                    AddPossibleCoords();
+                    brain.turnPhase = 1;
                 }
-                if (emptyNodesFiredAt == 2 || Ship.CheckForDestroyed(grids.playerGrid, shipsLeft))
-                {
-                    turnPhase = 1;
-                    break;
-                }
-                ShootAtFoundShip(grids);
                 break;
         }
 
 
+        
+        static Point GetRandomCoords(Brain _) => new(rng.Next(0, 8), rng.Next(0, 8));
 
-        // Adds all possible coords the ships could be in.
-        static void AddPossibleCoords()
+        // Returns the randomized coord pair with an alteration of 1 or -1 to one of them.
+        static Point GetAlteredCoords(Brain brain) =>  rng.Next(1, 3) > 1.5 ? 
+        brain.sucShots[0] with {Column = brain.sucShots[0].Column + rng.Next(1, 3) > 1.5 ? 1 : -1} :
+        brain.sucShots[0] with {Row = brain.sucShots[0].Row + rng.Next(1, 3) > 1.5 ? 1 : -1};
+
+        // Returns a random dequeue if both queues have the same amount of coords in them, else picks the one with the most and dequeues a pair.
+        static Point GetQueuedCoords(Brain brain) => brain.searchQueue[0].Count == brain.searchQueue[1].Count ? brain.searchQueue[rng.Next(0, 2)].Dequeue() : 
+        (brain.searchQueue[0].Count < brain.searchQueue[1].Count ? 
+        brain.searchQueue[1].Dequeue() : brain.searchQueue[0].Dequeue());
+
+
+        // Adds all possible coords the ship could be in.
+        static void AddPossibleCoords(Brain brain)
         { 
             for (int i = 1; i < 4; i++)
             {
-                coordSearchQueue[0].Enqueue(sucShots[0].Item1 == sucShots[1].Item1 ? (sucShots[0].Item1, sucShots[0].Item2 + i) : (sucShots[0].Item1 + i, sucShots[0].Item2));
-                coordSearchQueue[1].Enqueue(sucShots[0].Item1 == sucShots[1].Item1 ? (sucShots[0].Item1, sucShots[0].Item2 + i * -1) : (sucShots[0].Item1 + i * -1, sucShots[0].Item2));
+                brain.searchQueue[0].Enqueue(brain.sucShots[0].Column == brain.sucShots[1].Column ? new(brain.sucShots[0].Column, brain.sucShots[0].Row + i) : new(brain.sucShots[0].Column + i, brain.sucShots[0].Row));
+                brain.searchQueue[1].Enqueue(brain.sucShots[0].Column == brain.sucShots[1].Column ? new(brain.sucShots[0].Column, brain.sucShots[0].Row + i * -1) : new(brain.sucShots[0].Column + i * -1, brain.sucShots[0].Row));
             }
         }
     }
 
 
-    // This method is used when the ships's orientation has been found.
-    static void ShootAtFoundShip(Grid grids) // Could be improved by accounting for when the ai finds two ships next to each other.
+
+    // Repeats the desired function until it succeeds or the amount of times specified is met.
+    // If no limit is specified then it'll repeat until its attack function succeeds.
+    public static (Point point, bool shotShip) Loop(Func<Node[,], Point, bool> miscAttackFunction, Func<Brain, Point> obtainCoordinates, Node[,] grid, Brain brain, int times = 0)
     {
-        (int column, int row) coords;
+        int counter = 0;
+        Point point;
 
         do
         {
-            int randomQueue = rng.Next(0, 2);
-            coords = coordSearchQueue[randomQueue].Dequeue();
+            point = obtainCoordinates(brain);
+            counter += times == 0 ? -1 : 1;
+        } while (!miscAttackFunction(grid, point) && counter < times);
 
-            if (grids.playerGrid[coords.column, coords.row].NodeFilled != true)
-            {
-                emptyNodesFiredAt++;
-                coordSearchQueue[randomQueue].Clear();
-            }
-        } while (emptyNodesFiredAt < 2 || !Shoot(grids.playerGrid, coords));
-    }
-
-
-
-    // This method is for when one segment of a ship has been found. 
-    static bool FindShipRotation(Grid grids)
-    {
-        (List<int> column, List<int> row) = ([1, -1], [1, -1]);
-
-        while (true) // True since there is gauranteed to be a second ship segments if this method is called.
-        {
-            // Sets randomList to a random one if both have more than one element left in them, else it sets it to whichever one has elements left in it.
-            List<int> randomList = rng.Next(1, 5) > 1.5 ?
-            (column.Count > 0 ? ref column : ref row) : 
-            (row.Count > 0 ? ref row : ref column);
-
-            int randomIncrement = randomList[rng.Next(0, randomList.Count)];
-
-            if (Shoot(grids.playerGrid, randomList == column ? (coords.column + randomIncrement, coords.row) : (coords.column, coords.row + randomIncrement))) 
-            {    
-                // Returns true if it hit a ship segment, false if not.
-                return randomList == column ? 
-                grids.playerGrid[coords.column + randomIncrement, coords.row].NodeFilled == true : 
-                grids.playerGrid[coords.column, coords.row + randomIncrement].NodeFilled == true;
-            }
-    
-            randomList.Remove(randomIncrement); 
-        }
-    }
-
-
-
-    // Searching for ships by randomly firing.
-    static bool FireRandomly(Grid grids) // Searches randomly for ships.
-    {
-        do
-        {
-            (coords.column, coords.row) = (rng.Next(0, 8), rng.Next(0, 8));
-        } while (!Shoot(grids.playerGrid, coords));
-
-        return grids.playerGrid[coords.column, coords.row].NodeFilled == true;
+        return (point, grid[point.Column, point.Row].NodeFilled == true);
     }
 }
+
+
+
+class Brain()
+{
+    public readonly List<NodeTypes>[] shipsLeft = [[], [], [], [], [], []];
+    public readonly List<Point> sucShots = [];
+    public readonly Queue<Point>[] searchQueue = [[], []]; // The first queue is for positively modified coords while the second is for negatively.
+    public int turnPhase = 1;
+}
+
+
+
+record Point(int Column, int Row);
