af8bf53eedce2867836122059d7b0fa847bd8abb
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-07-25 21:30:36 -0500

    Added file to hold the the game flow in.

---
+++ Game.cs
@@ -0,0 +1,25 @@
+static class Game
+{
+    public static void Start()
+    {
+        bool gameActive = true;
+        Grid grids = new();
+        Brain brain = new(grids.player);
+        PlayerData playerData = new(grids.opponent);
+
+        while (gameActive)
+        {
+            grids.Refresh();
+            gameActive = Player.TakeTurn(ref grids, ref playerData);
+            Thread.Sleep(1500);
+            grids.Refresh();
+
+            if (gameActive)
+            {
+                gameActive = Opponent.TakeTurn(ref grids, ref brain);
+                Thread.Sleep(1500);
+                grids.Refresh();    
+            }      
+        }
+    }
+}
