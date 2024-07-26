362b6a54760b53ff397711fd700166747d08e703
Shiny Gyrodos <gibbs2kids@gmail.com>
2024-07-25 21:31:30 -0500

    Moved an extension method's logic to the constructor.

--- Grid.cs
+++ Grid.cs
@@ -2,26 +2,30 @@ class Grid
 {
     public Node[,] opponent = new Node[8, 8];
     public Node[,] player = new Node[8, 8];
+
+
+
+    public Grid()
+    {
+        for(int i = 0; i < 8; i++)
+        {
+            for(int j = 0; j < 8; j++)
+            {
+                opponent[i, j] = new Node(false, false, 'O', 0);
+                player[i, j] = new Node(false, false, 'O', 0);
+            }
+        }
+
+        Ship.PlaceAll(opponent, false);
+        Ship.PlaceAll(player, true);
+    }
 }
 
 
 
 static class GridExtensions
 {
-    public static void FillWithDefaultNodes(this Node[,] chosenGrid)
-    {
-        for(int i = 0; i < 8; i++)
-        {
-            for(int j = 0; j < 8; j++)
-            {
-                chosenGrid[i, j] = new Node(false, false, 'O', 0);
-            }
-        }
-    }
-
-
-
-    public static void Display(this Grid grid)
+    public static void Refresh(this Grid grid)
     {
         Console.Clear();
         Console.WriteLine("A B C D E F G H\n-----------------");
